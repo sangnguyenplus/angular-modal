08bce94d190f419752ace5908ee475dc5c1bf67b
Sang Nguyen <sangnguyenplus@gmail.com>
2016-07-22 14:28:14 -0400

    Initial repository

---
+++ js/modal.js
@@ -0,0 +1,88 @@
+angular.module('app', ['ui.bootstrap'])
+
+.controller('modal.alert', ['$scope', '$uibModalInstance', 'data', function($scope, $uibModalInstance, data) {
+    $scope.data = data;
+
+    $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+
+    $scope.close = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+}])
+
+.controller('modal.confirm', ['$scope', '$uibModalInstance', 'data', function($scope, $uibModalInstance, data) {
+    $scope.data = data;
+
+    $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+}])
+
+
+.service('appAlert', ['$uibModal', '$http', function($uibModal, $http) {
+    this.alert = function(data, callback) {
+        /*begin modal*/
+        var modalInstance = $uibModal.open({
+            templateUrl: '/views/alert.html',
+            controller: 'modal.alert',
+            backdrop: 'static',
+            resolve: {
+                data: function() {
+                    return data;
+                }
+            }
+        });
+    };
+
+    this.confirm = function(data, callback) {
+        /*begin modal*/
+        var modalInstance = $uibModal.open({
+            templateUrl: '/views/confirm.html',
+            controller: 'modal.confirm',
+            backdrop: 'static',
+            resolve: {
+                data: function() {
+                    return data;
+                }
+            }
+        });
+
+        modalInstance.result.then(function() {
+            return callback(true);
+        }, function() {
+            return callback(false);
+        });
+        /*end modal*/
+    };
+}])
+
+.controller('MainController', ['$scope', '$uibModal', 'appAlert', function($scope, $uibModal, appAlert) {
+    $scope.showModal = function(type) {
+        if (type == 'alert') {
+            appAlert.alert({
+                title: 'Title',
+                message: 'This is alert message!',
+                type: 'danger'
+            });
+        } else {
+            appAlert.confirm({
+                title: 'Confirm delete!',
+                message: 'Do you want to delete this record ?'
+            }, function(isOk) {
+                if (isOk) {
+                    appAlert.alert({
+                        title: 'Success',
+                        message: 'Delete record successfully!',
+                        type: 'success'
+                    });
+                }
+            });
+        }
+    };
+}]);
